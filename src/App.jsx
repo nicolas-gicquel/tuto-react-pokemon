803b81499a906787b3e1b500959d64011d510bc2
myrdinaptaliesen <64499780+myrdinaptaliesen@users.noreply.github.com>
2022-07-19 09:31:55 +0100

    Système de routage

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Blog from "./pages/Blog";
+import Home from "./pages/Home";
 
-function App() {
+
+const App = () => {
   return (
-    <div className="App">
-        <h1>Salut les dev!</h1>
-    </div>
+    <BrowserRouter>
+        <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="*" element={<Home />} />
+        </Routes>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
